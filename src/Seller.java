f526dc73185c9b6a0ff3c708efa9c7647472b1d7
Abdullah Salim <50784964+abdullahxsalim@users.noreply.github.com>
2024-05-18 22:35:31 +0600

    Add Seller

---
+++ src/Seller.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class Seller extends User {
+    private ArrayList<Product> productList;
+    public Seller(String name, String email, String password, String phone, String address, String city, int zip, String country) {
+        super(name, email, password, phone, address, city, zip, country);
+        this.productList = new ArrayList<Product>();
+    }
+    public void printInfo() {
+        System.out.println("Name: " + getName());
+        System.out.println("Email: " + getEmail());
+        System.out.println("Phone: " + getPhone());
+        System.out.println("Address: " + getAddress());
+        System.out.println("City: " + getCity());
+        System.out.println("Zip: " + getZip());
+        System.out.println("Country: " + getCountry());
+    }
+    public void addProduct(Product product) {
+        productList.add(product);
+    }
+    public void removeProduct(Product product) {
+        productList.remove(product);
+    }
+    public void updateProduct() {
+        // Update product in database
+    }
+    public void viewOrders() {
+        // View orders
+    }
+    public void viewProducts() {
+        // View products
+    }
+    public void viewProfile() {
+        // View profile
+    }
+    public void updateProfile() {
+        // Update profile
+    }
+
+}
