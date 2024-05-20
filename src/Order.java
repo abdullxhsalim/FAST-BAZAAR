5a6c7a199992abd5ea58e2fb2a227f1c7320cb79
Abdullah Salim <50784964+abdullahxsalim@users.noreply.github.com>
2024-05-20 10:25:34 +0600

    feat: Add orderedProducts and order details to Order class

--- src/Order.java
+++ src/Order.java
@@ -1,3 +1,18 @@
+import java.util.*;
+import java.time.*;
+
 public class Order {
-    
+    ArrayList<Product> orderedProducts;
+    private Buyer buyer;
+    private Seller seller;
+    private Courier courier;
+    private String status;
+    private String id;
+    private LocalDateTime orderPlacementTime;
+    private LocalDateTime orderDeliveryTime;
+    private double totalCost;
+    private String address;
+    private String city;
+    private int zip;
+    private String country;
 }
